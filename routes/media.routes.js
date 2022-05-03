2ead289111a10cd18cc867430dc3c19e77b5d32b
Leopoldo Navarrete <Leopoldo12@outlook.com>
2022-05-03 12:28:19 -0500

    Schema Model Changed

--- routes/media.routes.js
+++ routes/media.routes.js
@@ -1,49 +1,65 @@
+//REQUIRE VARIABLES
 const router = require("express").Router();
-const Media = require("../models/Media.model");
 const mongoose = require("mongoose");
 
+//IMPORT MODELS
+const Media = require("../models/Media.model");
+const Review = require("../models/Review.model");
+
+//ROUTER MEDIA
 router.get("/movies", (req, res, next) => {
   res.render("media/movies");
 });
 
+//ROUTER ANIME
 router.get("/anime", (req, res, next) => {
   res.render("media/anime");
 });
 
+//ROUTER MUSIC
 router.get("/music", (req, res, next) => {
   res.render("media/music");
 });
 
+//ROUTER DOCUMENTARIES
 router.get("/documentaries", (req, res, next) => {
   res.render("media/documentaries");
 });
 
+//ROUTER CREATE-NEW-MEDIA
 router.get("/create-new-media", (req, res, next) => {
   res.render("media/create-new-media");
 });
 
 router.post("/create-new-media", (req, res, next) => {
-  console.log("body =>", req.body)
+  console.log("body =>", req.body);
   Media.create(req.body)
-  .then( (newMedia) => {
-    console.log("New Media =>", newMedia)
-    if(newMedia.category === "Documentary"){
-      res.redirect("/media/documentaries")
-    } else if(newMedia.category === "Anime"){
-      res.redirect("/media/anime")
-    } else if(newMedia.category === "Music"){
-      res.redirect("/media/music")
-    } else if(newMedia.category === "Movie"){
-      res.redirect("/media/movies")
-    }
-  })
-  .catch( error => {
-    console.log("Este es el error =>", error)
-  })
+    .then((newMedia) => {
+      console.log("New Media =>", newMedia);
+      if (newMedia.category === "Documentary") {
+        res.redirect("/media/documentaries");
+      } else if (newMedia.category === "Anime") {
+        res.redirect("/media/anime");
+      } else if (newMedia.category === "Music") {
+        res.redirect("/media/music");
+      } else if (newMedia.category === "Movie") {
+        res.redirect("/media/movies");
+      }
+    })
+    .catch((error) => {
+      console.log("Este es el error =>", error);
+    });
 });
 
+//ROUTER CREATE-NEW-REVIEW
 router.get("/create-new-review", (req, res, next) => {
   res.render("media/create-new-review");
 });
 
+router.post("/create-new-review",  (req, res, next) =>{
+  
+})
+
+
+//EXPORTS
 module.exports = router;
